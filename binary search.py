bbcaa707de17ed474052e9893f168474d5c171b4
sajjad1024 <115224886+sajjad1024@users.noreply.github.com>
2022-10-07 22:26:28 -0700

    Update binary search.py

--- binary search.py
+++ binary search.py
@@ -1,28 +1,27 @@
-# Binary search
-print ("Enter value")
-def binarysearch(value:int):
-    list=[4,5,8,9,15,17]
+# binarysearch
+list = [4 , 5 , 6 , 8 , 9 , 15 , 17]
 
-    lowerbound=0
-    upperbound=len(list)
-    found=False
-    notfound=False
-    while found==False and notfound==False:
-        midpoint=int((lowerbound+upperbound)/2)
-        if list(midpoint)==value:
-            found=True
-        elif list(midpoint)>value:
-            lowerbound =midpoint + 1
-        elif list(midpoint)<value:
-            upperbound =midpoint - 1
-
-        if lowerbound>upperbound:
+def binarysearch(value):
+    lowerbound = 0
+    upperbound = len(list)-1
+    found = False
+    notfound = False
+    while found == False and notfound == False:
+        midpoint = int((lowerbound+upperbound)/2)
+        if lowerbound > upperbound:
             notfound=True
+            return -1
+        elif list[midpoint] == value:
+            found = True
+            return midpoint
+        elif list[midpoint] < value:
+            lowerbound = midpoint + 1
+        elif list[midpoint] > value:
+            upperbound = midpoint - 1
 
-    if found==True:
-        return("value is in the list")
-    else:
-        return("not found")
-
-
-
+value=int(input("enter a number: "))
+ans=binarysearch(value)
+if ans!=-1:
+    print("value is in the list")
+else:
+    print("not found")
