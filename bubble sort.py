49cf1049420eb29adea0ca0a487b011fec08e820
sajjad1024 <115224886+sajjad1024@users.noreply.github.com>
2023-03-12 11:39:40 +0500

    Update bubble sort.py

--- bubble sort.py
+++ bubble sort.py
@@ -13,3 +13,20 @@ def bubbleSort():
 
 
 bubbleSort()
+#efficient bubblesort
+array = [4,68,25,98,5,13,79,42,76,28]
+def effbubbleSort():
+    noswaps= False
+    boundary = len(array) - 1
+    while noswaps == False:
+        noswaps = True
+        for y in range(0,boundary):
+            if array[y] > array[y + 1]: #will use < sign for descending order
+                temp = array[y]
+                array[y] = array[y + 1]
+                array[y + 1] = temp
+                noswaps = False
+        boundary = boundary - 1
+    print(array)
+
+effbubbleSort()
